07bc1aff572559e8eb497fa098a5091494ab2f8c
ankor <inusahharis@gmail.com>
2019-01-23 16:24:49 +0000

    better way pf parsing ssid

--- wifi_active.py
+++ wifi_active.py
@@ -3,28 +3,32 @@ import os
 import subprocess
 import re
 
-# Still working on my naming skills (Dont judge me) );
+
+# Still working on my naming (Dont judge me) );
 
 
 def main():
     def sanitize_string(dirty_string):
         return dirty_string.replace(r'\n', '').strip()
 
-    # This one dirty way I can come up with to parse the text
     def _parse_ssid_string(ssid_str):
-        first_parse = re.search(r'.*(?=Mode)', ssid_str)
-        sec_parse = re.search(r'(?<=ESSID:).*', first_parse.group())
-        return sanitize_string(sec_parse.group())
+        re_ssid = re.compile(r"(?<=ESSID:).*(?=Mode)")
+        matches = re_ssid.findall(ssid_str)
+        ssid = ""
+        for match in matches:
+            ssid += sanitize_string(match)
+        return ssid
 
     def _check_current__ssid():
         try:
             ssid_class = subprocess.run("iwconfig", capture_output=True)
-            ssid_str = str(ssid_class.stdout)
-            return _parse_ssid_string(ssid_str)
         except IOError:
             raise Exception("Error checking wifi")
 
-    def _find_wifi_pass(ssid):
+        ssid_str = str(ssid_class.stdout)
+        return _parse_ssid_string(ssid_str)
+
+    def _find_wifi_password(ssid):
         try:
             os.system("sudo cat /etc/NetworkManager/system-connections/" +
                       ssid + " | grep psk=")
@@ -34,8 +38,8 @@ def main():
     if sys.platform.startswith("linux"):
         ssid = _check_current__ssid()
         if "off" not in ssid:
-            _find_wifi_pass(ssid)
-            print("Password for %s  is after psk above" % ssid)
+            _find_wifi_password(ssid)
+            print("Password for %s  is text after psk= " % ssid)
         else:
             print("Not connected to wifi")
     else:
